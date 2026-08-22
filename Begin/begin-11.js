358649beff0d6ea62912f5d1fbf258b9f1cd92d0
J-Fankourt <cacaozzzz.0@gmail.com>
2026-08-23 00:28:33 +0300

    feat: Done task 11

--- Begin/begin-11.js
+++ Begin/begin-11.js
@@ -1,3 +1,4 @@
+const PromptSync = require("prompt-sync")();
 
 let x = Math.abs(Number(PromptSync("Введите значение числа x: ")));
 let y = Math.abs(Number(PromptSync("Введите значение числа y: ")));
