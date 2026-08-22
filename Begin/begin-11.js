065ade60e888885635f83b955de31d7dabd0bd5b
J-Fankourt <cacaozzzz.0@gmail.com>
2026-08-23 00:26:54 +0300

    feat: Done task 11

---
+++ Begin/begin-11.js
@@ -0,0 +1,11 @@
+
+let x = Math.abs(Number(PromptSync("Введите значение числа x: ")));
+let y = Math.abs(Number(PromptSync("Введите значение числа y: ")));
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
