9c1cf37f823250f183477b8faf1ce8785eb78db2
J-Fankourt <cacaozzzz.0@gmail.com>
2026-08-24 23:39:08 +0300

    feat: Done task 12

---
+++ Begin/begin-12.js
@@ -0,0 +1,13 @@
+const PromptSync = require("prompt-sync")();
+
+let value_a = Number(PromptSync("Введите значение: "));
+let value_b = Number(PromptSync("Введите значение: "));
+
+console.log("Рассчитаем гипотенузу");
+
+let value_c = Math.hypot(value_a, value_b); 
+console.log(value_c);
+
+console.log("Рассчитаем периметр");
+
+console.log(value_a + value_b + value_c); 
