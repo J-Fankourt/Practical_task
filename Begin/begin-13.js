60f51a14801b043eee68294345fe14eb6034c776
J-Fankourt <cacaozzzz.0@gmail.com>
2026-08-25 00:35:07 +0300

    feat: Done task 13

--- Begin/begin-13.js
+++ Begin/begin-13.js
@@ -1,17 +1,16 @@
 const PromptSync = require("prompt-sync")();
 
-let radius_R1 = Number(PromptSync("Введите значение: "));
-let radius_R2 = Number(PromptSync("Введите значение: "));
-const π = 3.14;
+const radius_R1 = Number(PromptSync("Введите значение: "));
+const radius_R2 = Number(PromptSync("Введите значение: "));
 
 console.log("Рассчитаем площадь S1");
-let S1 = π * (radius_R1 * radius_R1); 
-console.log(S1);
+const area_S1 = Math.PI * (radius_R1 * radius_R1); 
+console.log(area_S1);
 
 console.log("Рассчитаем площадь S2");
-let S2 = π * (radius_R2 * radius_R2);
-console.log(S2);
+const area_S2 = Math.PI * (radius_R2 * radius_R2);
+console.log(area_S2);
 
 console.log("Рассчитаем площадь S3");
-let S3 = (S1 - S2);
-console.log(S3);
+const area_S3 = (area_S1 - area_S2);
+console.log(area_S3);
